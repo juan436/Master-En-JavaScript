6a82481ef37759278ff98583ddda81515747d6f0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-14 00:36:13 -0400

    Cuerto ejercicio completado: Mostrar numeros enteros entre los numeros ingresados

---
+++ Bloque01/04.js
@@ -0,0 +1,27 @@
+/*Programa que muestre todos los números enteros que hay entre dos números introducidos por el usuario*/
+
+let n1, n2;
+let numbers = [];
+
+do {
+    n1 = prompt("Ingrese un número: ");
+    n2 = prompt("Ingrese otro número: ");
+
+    if (isNaN(n1) || isNaN(n2)) {
+        alert("Debe ingresar solamente números");
+    }
+
+}while (isNaN(n1) || isNaN(n2));
+
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+
+let m = n1>n2 ? n2 : n1;
+let M  = n1>n2 ? n1 : n2;
+
+for(let i = m+1; i<M; i++){
+    numbers.push(i);
+}
+
+alert( `Los números entre ${n1} y ${n2} son: ${numbers}` );
