690274fe9e8c9f4eaa701a25587983ef7dd0ef41
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-14 16:39:59 -0400

    Sexto ejercicio finalizado

---
+++ Bloque01/06.js
@@ -0,0 +1,21 @@
+/*Mostrar todos los números divisores de un número introducido*/
+let n;
+let divisores = [];
+
+do{
+
+    n = prompt("Ingrese un número: ", 1);
+
+    if(isNaN(n)) alert("Debe ingresar solamente números");
+    else if (n<=0) alert("Debe ingresar un número mayor a 0");
+    
+
+} while(isNaN(n) || n<=0);
+
+for(let i=n; i>0; i--){
+
+    if(n%i==0){ divisores.push(i);}
+
+}
+
+alert(`Los divisores de ${n} son: ${divisores}`);
